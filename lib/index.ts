eecf86404d5b9317ec2bc2bb9461ba1e004b241e
梁柱 <ymblender@gmail.com>
2020-04-28 15:33:29 +0800

    feat: add moc

--- lib/index.ts
+++ lib/index.ts
@@ -3,36 +3,103 @@ import mermaid from "mermaid";
 import "./githubCss";
 
 const renderer = new marked.Renderer();
+const mermaidCache = {} as any;
 
-renderer.code = function(
+export interface MocOptions {
+  text: string;
+  raw: string;
+  level: number;
+  index: number;
+  line: number;
+  onClick?: Function;
+}
+
+export interface MarkdownElement extends HTMLDivElement {
+  moc: MocOptions[];
+}
+
+renderer.code = function (
   code: string,
   language: string | undefined,
   isEscaped: boolean
 ): string {
-  if (language === "g" || language === "graph") {
-    return `<div style="opacity: 0; transition: opacity 0.2s ease-out; min-height: 200px; padding:10px 0px; overflow:auto; border:0.6px solid #eeeeee; border-radius:6px; margin:10px 0px;" class="mermaid">${code}</div>`;
+  if (code && (language === "g" || language === "graph")) {
+    const lastHtml = mermaidCache[code];
+    if (lastHtml) {
+      return `<div style="opacity: 1; text-align:center; transition: opacity 0.1s ease-out; min-height: 10px; padding:10px 0px; overflow:auto; border:0.6px solid #eeeeee; border-radius:6px; margin:10px 0px;">${lastHtml}</div>`;
+    }
+
+    return `<div data-code="${code}" style="opacity: 0; text-align:center; transition: opacity 0.1s ease-out; min-height: 10px; padding:10px 0px; overflow:auto; border:0.6px solid #eeeeee; border-radius:6px; margin:10px 0px;" class="mermaid">${code}</div>`;
   }
   return "<pre><code>" + code + "</code></pre>";
 };
 
-function VanillaMarkdown(md: string) {
+function VanillaMarkdown(md: string, isGetMoc?: boolean) {
+  const moc: MocOptions[] = [];
 
-  const view = document.createElement('div');
-  view.className = 'vmdb';
-  view.style.overflowX = 'hidden';
+  if (isGetMoc) {
+    (window as any).vmd_moc_click = (i: number) => {
+      if (moc && moc[i] && moc[i].onClick) {
+        (moc as any)[i].onClick(moc[i].index, moc[i].line);
+      }
+    };
+
+    // 计算每个 header在第几行
+    const lines: number[] = [];
+    md.split("\n").forEach((v, i) => {
+      if (v[0] === "#") {
+        lines.push(i);
+      }
+      return v;
+    });
+
+    let index = -1;
+    renderer.heading = (text, level, raw, slugger) => {
+      index += 1;
+      moc.push({
+        text,
+        raw,
+        level,
+        index,
+        line: lines[index],
+      });
+      return `<h${level} onclick="vmd_moc_click(${index})" class="moc-${index}">${text}</h${level}>`;
+    };
+  }
+
+  const view = document.createElement("div") as MarkdownElement;
+  view.className = "vmdb";
+  view.style.overflowX = "hidden";
   view.style.padding = "20px 5% 60px 5%";
   view.innerHTML = marked(md, {
     renderer,
-    xhtml: true
+    xhtml: true,
   });
+  view.moc = moc;
 
-  setTimeout(()=>{
+  setTimeout(() => {
     try {
-      const nodeList = document.querySelectorAll(".mermaid");
-      mermaid.initialize({
-        securityLevel: "loose"
+      const nodeList = view.querySelectorAll(".mermaid");
+      // mermaid.parse(code)
+      let error: any;
+      nodeList.forEach((n) => {
+        try {
+          mermaid.parse(n.innerHTML);
+        } catch (err) {
+          error = err;
+          n.innerHTML = `<span style="color:#f55; text-align:center;">${err.str}</span>`;
+        }
       });
-      mermaid.init(nodeList);
+      if (!error) {
+        mermaid.init(nodeList);
+        nodeList.forEach((v) => {
+          const code = v.getAttribute("data-code");
+          const html = v.innerHTML;
+          if (code) {
+            mermaidCache[code] = html;
+          }
+        });
+      }
       nodeList.forEach((ele: any) => {
         ele.style.opacity = 1;
         ele.style.minHeight = "auto";
@@ -40,7 +107,7 @@ function VanillaMarkdown(md: string) {
     } catch (err) {
       console.warn("[mermaid]: ", err);
     }
-  }, 420);
+  }, 50);
 
   return view;
 }
--- umd/index.d.ts
+++ umd/index.d.ts
@@ -1,3 +1,14 @@
 import "./githubCss";
-declare function VanillaMarkdown(md: string): HTMLDivElement;
+export interface MocOptions {
+    text: string;
+    raw: string;
+    level: number;
+    index: number;
+    line: number;
+    onClick?: Function;
+}
+export interface MarkdownElement extends HTMLDivElement {
+    moc: MocOptions[];
+}
+declare function VanillaMarkdown(md: string, isGetMoc?: boolean): MarkdownElement;
 export default VanillaMarkdown;
